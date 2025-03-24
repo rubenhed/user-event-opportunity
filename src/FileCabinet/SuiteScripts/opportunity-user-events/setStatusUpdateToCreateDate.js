ceec31e5571665193b8ca9b97ac0194222e59340
Ruben <ruben.hedstrom@hotmail.com>
2025-03-24 14:56:01 +0900

    added set update date to created date function

---
+++ src/FileCabinet/SuiteScripts/opportunity-user-events/setStatusUpdateToCreateDate.js
@@ -0,0 +1,40 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType Suitelet
+ */
+
+// sets custbody_ga_status_update_date to datecreated for opportunities that don't have it set
+define(['N/record', 'N/search', 'N/log'],
+
+  (record, search, log) => {
+    const onRequest = (scriptContext) => {
+      const opportunitySearch = search.create({
+        type: search.Type.OPPORTUNITY,
+        columns: ['internalid', 'custbody_ga_status_update_date', 'datecreated']
+      });
+
+      opportunitySearch.run().each(result => {
+        const opportunityId = result.getValue({ name: 'internalid' });
+        const opportunityRecord = record.load({
+          type: record.Type.OPPORTUNITY,
+          id: opportunityId
+        });
+
+        const opportunityUpdateDate = result.getValue({ name: 'custbody_ga_status_update_date' });
+        if (opportunityUpdateDate) return true;
+
+        const opportunityCreateDate = result.getValue({ name: 'datecreated' });
+        opportunityRecord.setValue({
+          fieldId: 'custbody_ga_status_update_date',
+          value: new Date(opportunityCreateDate)
+        });
+
+        log.audit("details", `update date: ${opportunityUpdateDate}, create date: ${opportunityCreateDate}, id: ${opportunityId}`);
+        opportunityRecord.save();
+        return true;
+      });
+    }
+
+    return { onRequest }
+
+  });
